b016a0a09a16dcd9909fe2401e1648217d32f054
Adrien Etienne <a.etienne92@gmail.com>
2021-03-15 14:35:02 +0100

    Fix unit tests

--- test/ios/pages/home_test.dart
+++ test/ios/pages/home_test.dart
@@ -5,13 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:coconut_island_app/app/repositories/app_repository.dart';
 import 'package:coconut_island_app/ios/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Home page showing', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(App(
+      appRepository: AppRepository(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('Coconut Island'), findsOneWidget);
